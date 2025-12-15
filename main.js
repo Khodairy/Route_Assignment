3251ed1ded20d7bfb8012194dca2869e6bbeadac
Khodairy <Abdallakhodairy14@gmail.com>
2025-12-16 01:03:13 +0200

    Assginment 3 {part 1,part2 :{ get users, add new user}}

--- main.js
+++ main.js
@@ -1,309 +1,242 @@
-// // ========================= Assignment 1 (JavaScript Basics) =========================
+// ========================= Assignment 3 (Node.JS Basics) ==========================
 
-// // ===================================== Part 1 =======================================
-
-// // 1. Convert the string "123" to a number and add 7.
-// let strNum = "123";
-// console.log(Number(strNum) + 7); // Output: 130
-
-// // 2. Check if the given variable is falsy and return "Invalid" if it is.
-// let value = 0;
-// if (value == true) {
-//   console.log("Valid");
-// } else {
-//   console.log("Invalid");
-// } // Output: "Invalid"
-
-// // 3. Use for loop to print all numbers between 1 and 10, skipping even numbers using continue
-// let arrOddNumbers = [];
-// for (let i = 1; i <= 10; i++) {
-//   if (i % 2 === 0) {
-//     continue;
-//   }
-//   arrOddNumbers.push(i);
-// }
-// console.log(arrOddNumbers); // Output: [1, 3, 5, 7, 9]
-
-// // 4. Create an array of numbers and return only the even numbers using filter method.
-// let allNumbers = [1, 2, 3, 4, 5];
-// let arrEvenNumbers = allNumbers.filter((num) => {
-//   return num % 2 === 0;
-// });
-// console.log(arrEvenNumbers); // Output: [2, 4]
-
-// // 5. Use the spread operator to merge two arrays, then return the merged array.
-// let array1 = [1, 2, 3];
-// let array2 = [4, 5, 6];
-// console.log([...array1, ...array2]); // Output: [1, 2, 3, 4, 5, 6]
-
-// // 6. Use a switch statement to return the day of the week given a number (1 = Sunday ...., 7 = Saturday).
-// let dayNumber = 2;
-// switch (dayNumber) {
-//   case 1:
-//     console.log("Sunday");
-//     break;
-//   case 2:
-//     console.log("Monday");
-//     break;
-//   case 3:
-//     console.log("Tuesday");
-//     break;
-//   case 4:
-//     console.log("Wednesday");
-//     break;
-//   case 5:
-//     console.log("Thursday");
-//     break;
-//   case 6:
-//     console.log("Friday");
-//     break;
-//   case 7:
-//     console.log("Saturday");
-//     break;
-//   default:
-//     console.log("Invalid day number");
-// }
-
-// // 7. Create an array of strings and return their lengths using map method
-// let stringArray = ["a", "ab", "abc"];
-// let lengthsArray = stringArray.map((e) => {
-//   return e.length;
-// });
-// console.log(lengthsArray); // Output: [1, 2, 3]
-
-// // 8. Write a function that checks if a number is divisible by 3 and 5.
-// function isDivisibleBy3And5(num) {
-//   if (num % 3 === 0 && num % 5 === 0) {
-//     return "Divisible by both";
-//   } else {
-//     return "not Divisible by both";
-//   }
-// }
-// console.log(isDivisibleBy3And5(15)); // Output: "Divisible by both"
-
-// // 9. Write a function using arrow syntax to return the square of a number
-// let square = (e) => {
-//   return e ** 2;
-// };
-// console.log(square(5)); // Output: 25
-
-// // 10.Write a function that destructures an object to extract values and returns a formatted string.
-// const person = { name: "John", age: 25 };
-// function personInfo() {
-//   console.log(`${person.name} is ${person.age} years old.`);
-// }
-// personInfo(); // Output: "John is 25 years old."
-
-// // 11.Write a function that accepts multiple parameters (two or more) and returns their sum.
-// function sum(...nums) {
-//   return nums.reduce((a, b) => a + b, 0);
-// }
-// console.log(sum(1, 2, 3, 4, 5)); // Output: 15
-
-// // 12. Write a function that returns a promise which resolves after 3 seconds with a 'Success' message.
-// function delayedSuccess() {
-//   return new Promise((resolve) => {
-//     setTimeout(() => {
-//       console.log("Success");
-//     }, 3000);
-//   });
-// }
-// delayedSuccess(); // output: "Success" (after 3 seconds)
-
-// // 13. Write a function to find the largest number in an array.
-// function findLargesNumber(nums) {
-//   console.log(Math.max(...nums)); // Output: 9
-// }
-// findLargesNumber([1, 3, 7, 2, 4]); // Output: 7
-
-// // 14. Write a function that takes an object and returns an array containing only its keys.
-// let keysArray = [];
-// function getObjectKeys(obj) {
-//   for (ele in obj) {
-//     keysArray.push(ele);
-//   }
-//   return keysArray;
-// }
-// console.log(getObjectKeys({ name: "John", age: 30 })); // Output: ["name", "age"]
-
-// // 15. Write a function that splits a string into an array of words based on spaces.
-// function splitStringToWords(str) {
-//   console.log(str.split(" "));
-// }
-// splitStringToWords("The quick brown fox"); // Output: ['The', 'quick', 'brown', 'fox'
-
-// //=====================================================================================
-
-// // ===================================== Part 2 =======================================
-
-// // 1. What is the difference between forEach and for...of? When would you use each?
-// // for...of using for searching in arr or object by elements.
-// // forEach using for return new arr and modify part of arr.
-// // for(;;) using for searching in arr by index.
-// // for...in such as for(;;) searching by index.
-
-// //======================================
-
-// // 2. What is hoisting and what is the Temporal Dead Zone (TDZ)? Explain with examples.
-// // Hoisting: is a javaScript mechanism where variables and function declarations are moved to the top of their containing scope.
-// // Ex:
-// // hoistingFunction();
-// // function hoistingFunction() {
-// //   console.log("Hello From Hoisting Function");
-// // }
-// // TDZ: is the time between the entering of scope and the actual declaration.
-// // console.log(tdzVar); // outputCannot access 'tdzVar' before initialization
-// // let tdzVar = "I am outside TDZ";
-
-// //======================================
-
-// // 3. What are the main differences between == and ===?
-// // compare value only.
-// //compare value and type.
-
-// //======================================
-
-// // 4. Explain how try-catch works and why it is important in async operations.
-// // try-catch is used to handle errors in synchronous and asynchronous code.
-// // The code that may throw an error is placed inside the try block. If an error occurs,
-// // the control is transferred to the catch block where the error can be handled correctly.
-
-// //======================================
-
-// // 5. What’s the difference between type conversion and coercion? Provide examples of each.
-// // Type conversion is the process of converting a value from one type to another type.
-// // Example:
-// // let num = "123";
-// // let convertedNum = Number(num); // Explicit conversion from string to number
-// // console.log(convertedNum); // Output: 123
-
-// // Type coercion is the automatic converting values from one type to another.
-// // Example:
-// // let coercedNum = "5" - 2; // Implicit conversion from string to number
-// // console.log(coercedNum); // Output: 3
-
-// //=====================================================================================
-
-// // ===================================== Part 3 =======================================
-
-// // solution in bonus.js file
-
-//=====================================================================================
-//=====================================================================================
-//=====================================================================================
-
-// =========================== Assignment 2 (NodeJS Basics) ===========================
-
-// ===================================== Part 1 =======================================
-
-// 1. Write a function that logs the current file path and directory.
 const path = require("node:path");
-console.log(
-  `File:${path.join(
-    path.dirname(__filename),
-    path.basename(__filename)
-  )} & Dir:${path.dirname(__filename)}`
-); // Output: File:C:\Users\win\Desktop\Route_Assignment\main.js & Dir:C:\Users\win\Desktop\Route_Assignment
-
-// 2. Write a function that takes a file path and returns its file name.
-console.log(path.basename("/user/files/report.pdf")); // Output: report.pdf
-
-// 3. Write a function that builds a path from an object.
-console.log(path.format({ dir: "/folder", name: "app", ext: ".js" })); // Output: /folder\app.js
-
-// 4. Write a function that returns the file extension from a given file path.
-console.log(path.extname("/docs/readme.md")); // Output: .md
-
-// 5. Write a function that parses a given path and returns its name and ext.
-let { name, ext } = path.parse("/home/app/main.js");
-console.log({ name, ext }); // Output: { name: 'main', ext: '.js' }
-
-// 6. Write a function that checks whether a given path is absolute.
-console.log(path.isAbsolute("/home/user/file.txt")); // Output: true
-
-// 7. Write a function that joins multiple segments.
-console.log(path.join("src", "components", "App.js")); // Output: src\components\App.js
-
-// 8. Write a function that resolves a relative path to an absolute one.
-console.log(path.resolve(__filename)); // Output: C:\Users\win\Desktop\Route_Assignment\main.js
-
-// 9. Write a function that joins two paths.
-console.log(path.join("/folder1", "folder2/file.txt")); // Output: \folder1\folder2\file.txt
-
-// 10. Write a function that deletes a file asynchronously.
 const fs = require("node:fs");
 
-// fs.rm(path.resolve("./data.txt"), { recursive: true }, (error) => {
-//   if (error) {
-//     console.log(error);
-//   }
+// ===================================== Part 1 ========================================
+
+// 1. Use a readable stream to read a file in chunks and log each chunk.
+
+// let stream = fs.createReadStream(path.resolve("./data.txt"), {
+//   highWaterMark: 50,
+// });
+// stream.on("data", (chunk) => {
+//   console.log(chunk.toString());
 // });
 
-// 11. Write a function that creates a folder synchronously.
-fs.mkdirSync("src/folder/images", { recursive: true }, (error) => {
-  if (error) {
-    console.log(error);
+// =====================================================================================
+
+// 2. Use readable and writable streams to copy content from one file to another.
+
+// let readstream = fs.createReadStream(
+//   path.resolve("./readData.txt"),
+//   { encoding: "utf-8" },
+//   {
+//     highWaterMark: 50,
+//   }
+// );
+// readstream.on("data", (chunk) => {
+//   console.log(chunk.toString());
+//   writeStream.write(chunk);
+// });
+
+// let writeStream = fs.createWriteStream(path.resolve("./writeData.txt"), {
+//   flags: "a",
+// });
+
+// writeStream.on("finish", () => {
+//   console.log("Stream is written successfully");
+// });
+
+// =====================================================================================
+
+// 3. Create a pipeline that reads a file, compresses it, and writes it to another file.
+// =========== make a compresses stream ===========
+// const zlib = require("node:zlib");
+
+// // =========== compresses stream ===========
+// let gzib = zlib.createGzip();
+
+// // =========== uncompressed stream ===========
+// const gunZib = zlib.createGunzip();
+
+// let readstream = fs.createReadStream(
+//   path.resolve("./readData.txt"),
+//   { encoding: "utf-8" },
+//   {
+//     highWaterMark: 50,
+//   }
+// );
+// readstream.on("data", (chunk) => {
+//   console.log(chunk.toString());
+// });
+
+// let writeStream = fs.createWriteStream(path.resolve("./writeData.txt"), {
+//   flags: "a",
+// });
+
+// readstream.pipe(gzib).pipe(gunZib).pipe(writeStream);
+
+// writeStream.on("finish", () => {
+//   console.log("Stream is written successfully");
+// });
+
+// =====================================================================================
+// =====================================================================================
+// =====================================================================================
+
+// ===================================== Part 2 ========================================
+// Simple CRUD Operations Using HTTP
+
+const http = require("node:http");
+const port = 4000;
+
+// ========= get users from JSON file =========
+let users = fs.readFileSync(path.resolve("./user.json"), { encoding: "utf-8" });
+users = JSON.parse(users);
+
+// ============== Handle Requests ==============
+let server = http.createServer((req, res) => {
+  // ========= GET Users =========
+  if (req.method === "GET" && req.url === "/user") {
+    res.writeHead(200, { "content-type": "application/json" });
+    res.write(JSON.stringify({ statusCode: 200, data: users }));
+    return res.end();
+  }
+  // ========= POST Users =========
+  else if (req.method === "POST" && req.url === "/user") {
+    // ====== get the user witch added for check ====
+    let newUser = ``;
+    req.on("data", (chunk) => {
+      newUser += chunk;
+    });
+
+    req.on("end", () => {
+      let { id, name, email, age } = JSON.parse(newUser);
+
+      // ========== check if the user found in json file ========
+      let userExist = users.find((user) => {
+        return user.email === email;
+      });
+      if (userExist) {
+        // ======== if the user found =========
+        res.writeHead(409, { "content-type": "application/json" });
+        res.write(
+          JSON.stringify({
+            statusCode: 409,
+            message: "the user is already exist",
+          })
+        );
+        return res.end();
+      } else {
+        // ======== if the user not found =======
+        // ===== add user to users array =====
+        users.push({ id, name, email, age });
+        // ===== add user to json file =======
+        fs.writeFileSync(
+          path.resolve("./user.json"),
+          JSON.stringify(users, null, 2),
+          {
+            encoding: "utf-8",
+          }
+        );
+        res.writeHead(200, { "content-type": "application/json" });
+        res.write(
+          JSON.stringify({
+            statusCode: 200,
+            message: "a new user added successfully",
+          })
+        );
+        return res.end();
+      }
+    });
+  }
+  // ========= UPDATE Users =========
+  else if (req.method === "PATCH" && req.url === "/user") {
+    // ====== get the user witch added for check ====
+    let newUser = ``;
+    req.on("data", (chunk) => {
+      newUser += chunk;
+    });
+
+    req.on("end", () => {
+      let parsingNewUser = ``;
+      parsingNewUser = JSON.parse(newUser);
+
+      // ========== check if the user found in json file ========
+      let userExist = users.find((user) => {
+        return user.email === parsingNewUser.email;
+      });
+      if (userExist) {
+        // ======== if the user found =========
+        res.writeHead(409, { "content-type": "application/json" });
+        res.write(
+          JSON.stringify({
+            statusCode: 409,
+            message: "the user is already exist",
+          })
+        );
+        return res.end();
+      } else {
+        // ======== if the user not found =======
+        // ===== add user to users array =====
+        users.push(parsingNewUser);
+        // ===== add user to json file =======
+        fs.writeFileSync(
+          path.resolve("./user.json"),
+          JSON.stringify(users, null, 2),
+          {
+            encoding: "utf-8",
+          }
+        );
+        res.writeHead(200, { "content-type": "application/json" });
+        res.write(
+          JSON.stringify({
+            statusCode: 200,
+            message: "a new user added successfully",
+          })
+        );
+        return res.end();
+      }
+    });
+  }
+  // ========= GET 404 Page =========
+  else {
+    res.writeHead(404, { "content-type": "application/json" });
+    res.write(
+      JSON.stringify({ statusCode: 404, message: "page not found..." })
+    );
+    return res.end();
   }
 });
 
-// 12. Create an event emitter that listens for a "start" event and logs a welcome message.
-const EventEmitter = require("node:events");
-const { encode } = require("node:querystring");
-const { error } = require("node:console");
-const event = new EventEmitter();
-event.on("start", () => {
-  console.log("Welcome event triggered!");
+// ============== Handle listen to server ==============
+server.listen(port, () => {
+  console.log(`Server is running on ${port}`);
 });
-event.emit("start"); // Output: Welcome event triggered!
 
-// 13. Emit a custom "login" event with a username parameter.
-event.on("login", (name) => {
-  console.log(`User logged in: ${name}`);
+// ============== Handle Close server ==============
+server.on("close", () => {
+  console.log("Server is closed");
+
+  fs.writeFileSync(
+    path.resolve("./logs.txt"),
+    `\nServer is closed at ${new Date().toLocaleString()}`,
+    { flags: "a" }
+  );
 });
-event.emit("login", "Abdallah"); // Output: User logged in: Abdallah
 
-// 14. Read a file synchronously and log its contents.
-
-const data = fs.readFileSync(
-  path.resolve("./data2.txt"),
-  { encoding: "utf8" },
-  (error, data) => {
-    if (error) {
-      return error;
-    } else {
-      return data;
-    }
+// ============== Handle Errors ==============
+server.on("error", (error) => {
+  if (error.code === "EADDRINUSE") {
+    console.log("Port is in use, trying again...");
+    setTimeout(() => {
+      server.close();
+      server.listen(port);
+    }, 1000);
+  } else {
+    console.error(error);
   }
-);
-console.log({ data });
+});
 
-// 15. Write asynchronously to a file.
-fs.writeFile(
-  path.resolve("./data2.txt"),
-  "hello world ",
-  { flag: "a" },
-  (error) => {
-    if (error) {
-      console.log(error);
-    }
-  }
-);
+// =====================================================================================
 
-// 16. Check if a directory exists.
-
-if (fs.existsSync("./src/folder/images")) {
-  console.log("Directory exists");
-} else {
-  console.log("Directory does not exist");
-}
-// 17. Write a function that returns the OS platform and CPU architecture.
-
-//=====================================================================================
-
-// ===================================== Part 2 =======================================
+// ===================================== Part 3 ========================================
 
 // solution in bonus.js file
 
-//=====================================================================================
-//=====================================================================================
-//=====================================================================================
+// ===================================== Part 4 ========================================
+
+// solution in bonus.js file
+
+//======================================================================================
